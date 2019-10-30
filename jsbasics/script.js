a629b1cc8ee9fc521ccfe5233f8e8525f9a1eed9
Anjey1989 <andrejssavins@gmail.com>
2019-10-30 21:30:04 +0200

    29.10.19

---
+++ jsbasics/script.js
@@ -0,0 +1,40 @@
+console.log("hello world");
+
+var myName = "nosaukums";
+
+console.log(myName);
+
+console.log(typeof (myName));
+
+myName=100;
+
+console.log (typeof(myName));
+
+const mansVards = "Andrejs";
+const mans_vards = "Andrejs";
+
+// mansVards = "nav andrejs";
+// console.log(mansVards);
+
+var a;
+
+console.log(a);
+
+a = 7;
+
+var b = a;
+
+console.log(b);
+
+var q = 1; 
+var w = 2;
+var e = "I am a string";
+
+var z = "6";
+console.log(z+1);
+
+q= q*4;
+console.log(q);
+
+// q=q^3;
+// console.log(q);
